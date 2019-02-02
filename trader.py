6e500ddecd8c82cd442d29417ad3b28126923ba9
Taylor Joseph Savage <tjsavage@stanford.edu>
2019-02-01 16:16:26 -0800

    now sets a series of orders below and above market price, 2k ish

--- trader.py
+++ trader.py
@@ -2,6 +2,7 @@ import signal
 import requests
 import time
 from time import sleep
+import math
 
 import pandas as pd
 import numpy as np
@@ -21,12 +22,14 @@ base_path = '/v1'
 base_url = host_url + base_path
 
 sec = "ALGO"
-spread = 0.00
+default_spread = 0.02
 buy_volume = 2000
 sell_volume = 2000
 start_time = 295
 stop_time = 5
 max_order_size = 5000
+lag = 0.5
+limit_stock = 25000
 
 class ApiException(Exception):
     pass
@@ -61,35 +64,58 @@ def main():
             sec_best_bid = book.get_best_bid(ses, sec)['price']
             sec_best_ask = book.get_best_ask(ses, sec)['price']
 
+            spread = default_spread
+
             sec_data = securities.security_dict(ses, ticker_sym=sec)[sec]
+            sec_last = sec_data.last
+            position = sec_data.position
 
             o = orders.orders_dict(ses)
 
-            if len(o) != 2 and len(o) != 0:
-                print("Cancelling orders")
-                broker.cancel_order_bulk(ses, "<","0","<","0",all_flag=1)
-                sleep(.5)
-                continue
+            broker.cancel_order_bulk(ses, "<","0","<","0",all_flag=1)
+
+            max_buy_volume = limit_stock / 2 - position
+            max_sell_volume = limit_stock / 2 + position
+
+            bids = []
+            asks = []
+
+            # Buy orders            
+            for i in range(-5, 0):
+                curr_price = sec_last + i / 100
+                vol_to_buy = math.trunc(max_buy_volume / 5)
+
+                while vol_to_buy > 0:
+                    vol = min(max_order_size, vol_to_buy)
+                    bids.append((vol, curr_price))
+
+                    vol_to_buy -= vol
             
-            if len(o) == 0 or sec_data.position != 0:
-                new_bid = sec_best_bid + spread
-                new_ask = sec_best_ask - spread
+            # Sell orders
+            for i in range(1, 6):
+                curr_price = sec_last + i / 100
+                vol_to_sell = math.trunc(max_sell_volume / 5)
 
-                buy_amount = buy_volume - int(sec_data.position / 2)
-                sell_amount = sell_volume + int(sec_data.position / 2)
+                while vol_to_sell > 0:
+                    vol = min(max_order_size, vol_to_sell)
+                    asks.append((vol, curr_price))
 
-                buy_amount = min(max_order_size, buy_amount)
-                sell_amount = min(max_order_size, sell_amount)
+                    vol_to_sell -= vol
 
-                print("Position: %d" % sec_data.position)
-                print("Submitting buy @ %dx%f, sell @ %dx%f" % (buy_amount, new_bid, sell_amount, new_ask))
-                
-                if buy_amount > 0:
-                    broker.limit_order(ses, sec, 'BUY', buy_amount, new_bid)
-                if sell_amount > 0:
-                    broker.limit_order(ses, sec, 'SELL', sell_amount, new_ask)
 
-                sleep(0.1)
+            print("Position: %d" % position)
+            #print("Asks: " + asks)
+            #print("Bids: " + bids)
+
+            for bid in bids:
+                q, p = bid
+                broker.limit_order(ses, sec, 'BUY', q, p)
+            
+            for ask in asks:
+                q, p = ask
+                broker.limit_order(ses, sec, 'SELL', q, p)
+
+            sleep(lag)
 
             current_case = cases.case(ses)
             tick = current_case.tick
